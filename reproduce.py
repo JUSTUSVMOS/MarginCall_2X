e7d3c253f28741748fc1b9b2101b68493b6c136b
JUSTUSVMOS <vmos1018@gmail.com>
2026-04-20 09:53:53 +0800

    chore: cleanup repository hygiene and update .gitignore

--- reproduce.py
+++
@@ -1,6 +0,0 @@
-from engine_technical import build_technical_snapshot
-try:
-    print(build_technical_snapshot("CRWV"))
-except Exception as e:
-    import traceback
-    traceback.print_exc()
--- reproduce_crwv.py
+++
@@ -1,4 +0,0 @@
-from src.agent import execute_single_task
-import asyncio
-from engine_router import get_technical_analysis
-print(get_technical_analysis("CRWV", "1d"))
